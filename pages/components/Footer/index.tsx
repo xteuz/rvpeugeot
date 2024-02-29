94d0783144b122f88e39b713210697949136ab1e
xteuz <m4th3usb4tist4@gmail.com>
2024-02-29 19:29:34 -0300

    feated

--- pages/components/Footer/index.tsx
+++ pages/components/Footer/index.tsx
@@ -5,7 +5,7 @@ type FooterProps = {
 }
 const Footer = ({children}:FooterProps) => {
     return (
-        <footer className='bg-gray-950'>
+        <footer className='bg-black'>
             <div className='container mx-auto justify-center font-sans  items-center flex flex-col'>
                 {children}
             </div>
